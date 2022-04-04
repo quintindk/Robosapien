ed2db2783e99b7ef3cb3cb961838ddc71ef22818
Quintin de Kok <quintindk@tangentsolutions.co.za>
2022-04-04 10:59:34 +0200

    removed raw_input (python2)

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import robo
 
 rs=robo.Robo(21)	#create Robo object for GPIO 21
 rs.send_code(0xB1)	#Issue reset command
-raw_input('Enter')
+input("press enter")
 rs.send_code(0x81)	#Right arm up
 rs.send_code(0x81)
 rs.send_code(0x82)	#Right wrist out
@@ -14,7 +14,7 @@ rs.send_code(0x85)	#Right wrist in
 rs.send_code(0x82)
 rs.send_code(0x85)
 
-raw_input('Enter')
+input("press enter")
 rs.send_code(0x89)	#Left arm up
 rs.send_code(0x89)
 for i in range(0,2):
@@ -27,11 +27,11 @@ rs.send_code(0x84)	#Right arm down
 rs.send_code(0x8C)
 rs.send_code(0x84)
 
-raw_input('Enter')
+input("press enter")
 rs.send_code(0xC4)	#Hi 5
 
-raw_input('Enter')
+input("press enter")
 rs.send_code(0xCE)	#Roar
 
-print ("fin")
+print ("finished")
 
